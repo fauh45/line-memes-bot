feb5608ab17cab7495772c87a493b12cfffe6445
Fauzan Lubis <fauzanfauh45@gmail.com>
2020-06-17 07:58:45 +0700

    More Testing

--- app.py
+++ app.py
@@ -12,11 +12,15 @@ from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
+from linebot.models.sources import (
+    SourceUser, SourceGroup
+)
 from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+    MessageEvent, TextMessage, TextSendMessage,
+    LocationMessage, FollowEvent, ImageSendMessage
 )
 
 app = Flask(__name__)
@@ -51,21 +55,65 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def text_handler(event):
     text_content = event.message.text
+    text_content_list = text_content.split(' ')
+    specials = False
 
-    print("[INFO] Got text %s" % text_content)
-    if text_content.split(' ')[0] == '/memes':
+    if text_content_list[0] == '/memes':
+        text = "Not done yet mate\n"
+        if len(text_content_list > 1):
+            if text_content_list[1] == 'rick':
+                specials = True
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    ImageSendMessage(
+                        original_content_url="https://i.kym-cdn.com/entries/icons/original/000/023/608/picklerick.jpg"
+                    )
+                )
+
+            elif text_content_list[1] == 'nice':
+                specials = True
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    ImageSendMessage(
+                        original_content_url="https://i.kym-cdn.com/entries/icons/original/000/032/180/cover4.jpg"
+                    )
+                )
+
+        if not specials:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Umm, not done yet mate...\n\n"
+                                     "Contact fauh45 for more info")
+            )
+
+    elif isinstance(event.source, SourceGroup):
+        if text_content_list[0] == '/bye':
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Goodbye, we had a good run...")
+            )
+            line_bot_api.leave_group(event.source.group_id)
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def location_handler(event):
+    if isinstance(event.source, SourceUser):
+        lat, lon = event.latitude, event.longitude
+        text = "Lat = " + str(lat) + "\nLon=" + str(lon) + "\n\nContacting the FBI...."
+
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="Umm, not done yet mate...\n\n"
-                                 "Contact fauh45 for more info")
+            TextSendMessage(text=text)
         )
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Not understood,"
-                                 "Umm, not done yet mate...,"
-                                 "Contact fauh45 for more info")
-        )
+
+
+@handler.add(FollowEvent)
+def follow_handler(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="Hey!\nTo use this bot, try :\n/memes - for memes"
+                             "\n\nfauh45")
+    )
 
 
 if __name__ == '__main__':
